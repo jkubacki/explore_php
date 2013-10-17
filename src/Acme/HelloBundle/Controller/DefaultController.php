dda4c45b232868d59338e237b07f3a58f75a0773
kuba <kuba@kuba-laptop>
2013-10-17 19:56:45 +0200

    Gogi counting fun

--- src/Acme/HelloBundle/Controller/DefaultController.php
+++ src/Acme/HelloBundle/Controller/DefaultController.php
@@ -3,11 +3,19 @@
 namespace Acme\HelloBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('AcmeHelloBundle:Default:index.html.twig', array('name' => $name));
-    }
+
+	public function indexAction($name)
+	{
+		return $this->render('AcmeHelloBundle:Default:index.html.twig', array('name' => $name));
+	}
+
+	public function countingAction($from, $to)
+	{
+		return $this->render('AcmeHelloBundle:Default:counting.html.twig', array('from' => $from, 'to' => $to));
+	}
+
 }
